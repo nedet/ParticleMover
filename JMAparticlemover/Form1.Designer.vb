6f9f8cc4010a6a3b7d1265f882ad30a1eb646964
nedet <49131786+nedet@users.noreply.github.com>
2019-05-07 11:03:57 -0400

    Add files via upload

---
+++ JMAparticlemover/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class JMAParticleMover
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(JMAParticleMover))
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.tmrA = New System.Windows.Forms.Timer(Me.components)
+        Me.Btnstop = New System.Windows.Forms.Button()
+        Me.LblCount = New System.Windows.Forms.Label()
+        Me.btnStep = New System.Windows.Forms.Button()
+        Me.btnMake = New System.Windows.Forms.Button()
+        Me.picBall = New System.Windows.Forms.PictureBox()
+        Me.PicA = New System.Windows.Forms.PictureBox()
+        Me.picsquare = New System.Windows.Forms.PictureBox()
+        Me.picBar = New System.Windows.Forms.PictureBox()
+        Me.PicBall3 = New System.Windows.Forms.PictureBox()
+        CType(Me.picBall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicA, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picsquare, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicBall3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(52, 20)
+        Me.btnStart.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(56, 35)
+        Me.btnStart.TabIndex = 1
+        Me.btnStart.Text = "start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'tmrA
+        '
+        Me.tmrA.Interval = 2
+        '
+        'Btnstop
+        '
+        Me.Btnstop.Location = New System.Drawing.Point(117, 20)
+        Me.Btnstop.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Btnstop.Name = "Btnstop"
+        Me.Btnstop.Size = New System.Drawing.Size(57, 35)
+        Me.Btnstop.TabIndex = 3
+        Me.Btnstop.Text = "stop"
+        Me.Btnstop.UseVisualStyleBackColor = True
+        '
+        'LblCount
+        '
+        Me.LblCount.AutoSize = True
+        Me.LblCount.Location = New System.Drawing.Point(771, 34)
+        Me.LblCount.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LblCount.Name = "LblCount"
+        Me.LblCount.Size = New System.Drawing.Size(0, 20)
+        Me.LblCount.TabIndex = 6
+        '
+        'btnStep
+        '
+        Me.btnStep.Location = New System.Drawing.Point(513, 20)
+        Me.btnStep.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnStep.Name = "btnStep"
+        Me.btnStep.Size = New System.Drawing.Size(88, 35)
+        Me.btnStep.TabIndex = 8
+        Me.btnStep.Text = "Step"
+        Me.btnStep.UseVisualStyleBackColor = True
+        '
+        'btnMake
+        '
+        Me.btnMake.Location = New System.Drawing.Point(216, 15)
+        Me.btnMake.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnMake.Name = "btnMake"
+        Me.btnMake.Size = New System.Drawing.Size(140, 43)
+        Me.btnMake.TabIndex = 9
+        Me.btnMake.Text = "Make Particle"
+        Me.btnMake.UseVisualStyleBackColor = True
+        '
+        'picBall
+        '
+        Me.picBall.Image = Global.SimTest1.My.Resources.Resources.ball2
+        Me.picBall.Location = New System.Drawing.Point(998, 134)
+        Me.picBall.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.picBall.Name = "picBall"
+        Me.picBall.Size = New System.Drawing.Size(30, 31)
+        Me.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBall.TabIndex = 7
+        Me.picBall.TabStop = False
+        Me.picBall.Visible = False
+        '
+        'PicA
+        '
+        Me.PicA.BackColor = System.Drawing.Color.Beige
+        Me.PicA.Location = New System.Drawing.Point(30, 92)
+        Me.PicA.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.PicA.Name = "PicA"
+        Me.PicA.Size = New System.Drawing.Size(1072, 660)
+        Me.PicA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicA.TabIndex = 0
+        Me.PicA.TabStop = False
+        '
+        'picsquare
+        '
+        Me.picsquare.Image = CType(resources.GetObject("picsquare.Image"), System.Drawing.Image)
+        Me.picsquare.Location = New System.Drawing.Point(964, 189)
+        Me.picsquare.Name = "picsquare"
+        Me.picsquare.Size = New System.Drawing.Size(100, 100)
+        Me.picsquare.TabIndex = 10
+        Me.picsquare.TabStop = False
+        '
+        'picBar
+        '
+        Me.picBar.Image = CType(resources.GetObject("picBar.Image"), System.Drawing.Image)
+        Me.picBar.Location = New System.Drawing.Point(750, 331)
+        Me.picBar.Name = "picBar"
+        Me.picBar.Size = New System.Drawing.Size(34, 149)
+        Me.picBar.TabIndex = 11
+        Me.picBar.TabStop = False
+        '
+        'PicBall3
+        '
+        Me.PicBall3.Image = CType(resources.GetObject("PicBall3.Image"), System.Drawing.Image)
+        Me.PicBall3.Location = New System.Drawing.Point(1035, 134)
+        Me.PicBall3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.PicBall3.Name = "PicBall3"
+        Me.PicBall3.Size = New System.Drawing.Size(30, 31)
+        Me.PicBall3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicBall3.TabIndex = 12
+        Me.PicBall3.TabStop = False
+        Me.PicBall3.Visible = False
+        '
+        'JMAParticleMover
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1125, 751)
+        Me.Controls.Add(Me.PicBall3)
+        Me.Controls.Add(Me.picBar)
+        Me.Controls.Add(Me.picsquare)
+        Me.Controls.Add(Me.btnMake)
+        Me.Controls.Add(Me.btnStep)
+        Me.Controls.Add(Me.picBall)
+        Me.Controls.Add(Me.LblCount)
+        Me.Controls.Add(Me.Btnstop)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.PicA)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "JMAParticleMover"
+        Me.Text = "JMAParticleMover"
+        CType(Me.picBall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicA, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picsquare, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicBall3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PicA As PictureBox
+    Friend WithEvents btnStart As Button
+    Friend WithEvents tmrA As Timer
+    Friend WithEvents Btnstop As Button
+    Friend WithEvents LblCount As Label
+    Friend WithEvents picBall As PictureBox
+    Friend WithEvents btnStep As Button
+    Friend WithEvents btnMake As Button
+    Friend WithEvents picsquare As PictureBox
+    Friend WithEvents picBar As PictureBox
+    Friend WithEvents PicBall3 As PictureBox
+End Class
---
+++ JMAparticlemover/Form1.vb
@@ -0,0 +1,235 @@
+﻿Public Class JMAParticleMover
+    'programmed by Jeremy Antonuccio in VB
+    '04/16/19
+
+    ' use Make Particle button a few times
+    ' press STEP button to see them move
+    ' only use START button if STEP is working
+    ' make up to 50 particles whenever desired
+
+    Dim ballw As Integer = 20           ' each particle is 20 by 20  
+    Dim ballh As Integer = ballw
+
+    Dim myParticle As New List(Of PictureBox)   '  holds array of particles
+    Dim randbox As New Random
+    Dim count As Integer = 0           ' counts particles
+    Dim j As Integer
+
+    Dim maxParticles As Integer = 50   'max number of particles
+
+    ' arrays to hold x, y, and velocities for each particle
+    Dim x(maxParticles), y(maxParticles) As Integer
+    Dim xvel(maxParticles), yvel(maxParticles) As Integer
+
+    Private Sub tmrA_Tick(sender As Object, e As EventArgs) Handles tmrA.Tick
+
+        moveStuff()
+
+    End Sub
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ballw = 10
+        ballh = 10
+
+        btnMake_Click(sender, e)    'creates ball 0
+
+        picsquare.Visible = False 'till code is finished
+        picBar.Visible = True
+    End Sub
+
+    Private Sub btnStep_Click(sender As Object, e As EventArgs) Handles btnStep.Click
+
+        moveStuff()
+
+    End Sub
+
+
+    Private Sub moveStuff()
+
+        For j = 0 To count - 1
+            x(j) = myParticle(j).Location.X + xvel(j)
+            y(j) = myParticle(j).Location.Y + yvel(j)
+
+
+
+            'make particle bounce off edges
+            If x(j) > (PicA.Width - 25) Then
+                If xvel(j) > 0 Then ' stop ball from getting stuck on edge
+                    xvel(j) *= -1
+                End If
+            ElseIf x(j) < 1 Then
+                If xvel(j) < 0 Then ' stop ball from getting stuck on edge
+                    xvel(j) *= -1
+                End If
+            End If
+            If y(j) > (PicA.Height - 28) Then
+                If yvel(j) > 0 Then ' stop ball from getting stuck on edge
+                    yvel(j) *= -1
+                End If
+            ElseIf y(j) < 1 Then
+                If yvel(j) < 0 Then ' stop ball from getting stuck on edge
+                    yvel(j) *= -1
+                End If
+            End If
+
+            collisionChecker()      ' checks particle j vs particle k
+
+            barchecker()    ' checks if any particle hits bar
+
+            myParticle(j).SetBounds(x(j), y(j), ballw, ballh)
+        Next j
+
+    End Sub
+
+    Private Sub Btnstop_Click(sender As Object, e As EventArgs) Handles Btnstop.Click
+        tmrA.Stop() 'stops timer
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        tmrA.Start()
+
+    End Sub
+
+    Private Sub collisionChecker()
+        Dim j, k, tempVel As Integer   'need local j and k
+
+        ' check each particle against higher numbered ones
+        ' if close enough to call a hit, change velocity of both.
+
+        For j = 0 To count - 2
+            For k = j + 1 To count - 1
+
+                ' check for collision
+                If hit(j, k) Then   ' call function to see if pj hit pk
+                    tempVel = xvel(j)
+                    xvel(j) = xvel(k)
+                    xvel(k) = tempVel
+                    '    swap all relevant velocities for pj And pk
+                    tempVel = yvel(j)
+                    yvel(j) = yvel(k)
+                    yvel(k) = tempVel
+
+                End If
+
+
+            Next k
+
+        Next j
+
+    End Sub
+
+    Private Function hit(jj As Integer, kk As Integer) As Boolean
+        ' this function finds distance between particles jj and kk
+
+        Dim distance, xdist, ydist, BGDistance As Single
+
+        ' calculate xdist as x(jj) - x(kk)
+        xdist = x(jj) - x(kk)
+
+        ' calulate  ydist as yy(jj) - y(kk)
+        ydist = y(jj) - y(kk)
+        ' calculate bigdistance as  xdist squared + ydist squared
+        BGDistance = xdist ^ 2 + ydist ^ 2
+        ' calculate distance as square root of bigdistance
+        distance = Math.Sqrt(BGDistance)
+        ' fake distance = randbox.Next(20, 100)
+
+
+
+        If distance < 20 Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
+    Private Sub barchecker()
+        ' checks if particle hits bar
+        Dim j, barleft, barright, bartop, barbottom, fudge As Integer   'need local j 
+
+        fudge = 5
+        barleft = picBar.Left - fudge - PicA.Location.X
+        barright = barleft + picBar.Width + fudge
+        bartop = picBar.Top - fudge - PicA.Location.Y
+        barbottom = bartop + picBar.Height + fudge
+
+        For j = 0 To count - 1
+            ' see if particle j's x is between bar's left-fudge and right+fudge
+            If xvel(j) < 0 Then
+
+                If x(j) > barleft And x(j) < barright Then
+                    If y(j) > bartop And y(j) < barbottom Then
+                        xvel(j) *= -1
+                        'yvel(j) *= -1
+                    End If
+                End If
+            Else
+                If x(j) + picBall.Width > barleft And x(j) + picBall.Width < barright Then
+                    If y(j) > bartop And y(j) < barbottom Then
+                        xvel(j) *= -1
+                    End If
+                End If
+
+            End If
+
+
+
+
+
+
+        Next j
+
+
+    End Sub
+
+
+    Private Sub btnMake_Click(sender As Object, e As EventArgs) Handles btnMake.Click
+        ' makes new picture of particle and adds to array list
+
+        If count < maxParticles Then ' sets max limit of particles
+
+            Dim tempPicBox As New PictureBox()
+            tempPicBox.Parent = PicA
+
+            ' make the left and top values random, so the particles will
+            ' likely appear at new locations
+
+            tempPicBox.Left = randbox.Next(40, 600) '300
+            tempPicBox.Top = randbox.Next(40, 400) '150
+
+            'DEBUG tempPicBox.Left = randbox.Next(40, 100)
+            'DEBUG tempPicBox.Top = 100
+            If randbox.Next(1, 3) = 1 Then
+                tempPicBox.Image = picBall.Image
+            Else
+                tempPicBox.Image = PicBall3.Image
+            End If
+            tempPicBox.SizeMode = PictureBoxSizeMode.StretchImage
+            tempPicBox.Visible = True
+
+            tempPicBox.Width = ballw
+            tempPicBox.Height = ballh
+
+            myParticle.Add(tempPicBox)    'creates another ball in array
+
+            xvel(count) = randbox.Next(-10, 11) ' random x speeds -- change ??
+            yvel(count) = randbox.Next(-5, 6) ' random y speeds
+
+            'DEBUG xvel(count) = randbox.Next(-10, 11) 
+            'DEBUG yvel(count) = 0
+
+            count += 1
+
+            LblCount.Text = count
+
+
+        End If
+    End Sub
+
+End Class
